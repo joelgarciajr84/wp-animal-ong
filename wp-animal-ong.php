ae1eaed1d9143d42da8318218d64b8917a3c3e4f
joelgarciajr84 <joel.garciajr84@gmail.com>
2015-06-21 16:35:14 -0300

    Permissoes lancamento de caixa

--- wp-animal-ong.php
+++ wp-animal-ong.php
@@ -1,22 +1,26 @@
 <?php
 /*
- * Plugin Name: WP Animal ONG
- * Plugin URL: https://github.com/joelgarciajr84/wp-animal-ong
- * Description: Um plugin WordPress para auxiliar na gestão de ONG`s que resgatam e cuidam de animais.
- * Author: Joel Garcia Jr
- * Version: 1.0
- * Text Domain: wpanimal
- * Author URL: joel.garciajr84@gmail.com
- */
+	* Plugin Name: WP Animal ONG
+	* Plugin URL: https://github.com/joelgarciajr84/wp-animal-ong
+	* Description: Um plugin WordPress para auxiliar na gestão de ONG`s que resgatam e cuidam de animais.
+	* Author: Joel Garcia Jr
+	* Version: 1.0
+	* Text Domain: wpanimal
+	* Author URL: joel.garciajr84@gmail.com
+	* 
+*/
+if ( ! defined( 'ABSPATH' ) ) {		
+	exit; // Exit if accessed directly.		
+}
 
 register_activation_hook( __FILE__, 'setPermissions' );
 
 //Invocacoes Necessarias
+
 require_once dirname(__FILE__) . '/cfgs/permissoes.php';
 require_once dirname(__FILE__) . '/cfgs/menu-esquerdo.php';
 require_once dirname(__FILE__) . '/cpts/cpts.php';
 require_once dirname(__FILE__) . '/cfgs/cfgs.php';
-
 
 #Importacao dos javascripts necessarios
 add_action('admin_enqueue_scripts', 'jsnecessarios');
