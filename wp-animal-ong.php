53dd3660c0ebfdb32986ff8a8935e12820844fcb
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-01-29 15:49:37 -0200

    Update wp-animal-ong.php

--- wp-animal-ong.php
+++ wp-animal-ong.php
@@ -4,7 +4,7 @@
 	* Plugin URL: https://github.com/joelgarciajr84/wp-animal-ong
 	* Description: Um plugin WordPress para auxiliar na gestão de ONG`s que resgatam e cuidam de animais.
 	* Author: Joel Garcia Jr
-	* Version: 1.0
+	* Version: 1.1
 	* Text Domain: wpanimal
 	* Author URL: joel.garciajr84@gmail.com
 	* 
